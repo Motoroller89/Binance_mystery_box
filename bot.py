ea0d061af356fe11c1a3baf926fb067ba0bf42ae
telegrambotbinacne <danya.garayan@gmail.com>
2022-04-06 19:50:35 +0300

    refereal

--- bot.py
+++ bot.py
@@ -25,30 +25,32 @@ logging.basicConfig(level=logging.INFO)
 async def on_statrap(dp):
     await bot.set_webhook(URL_APP)
 
+
 async def on_shourdown(dp):
     await bot.delete_webhook()
 
+
 @dp.message_handler(commands="start")
 async def cmd_start(message: types.Message):
-
     try:
-        db.add_subscriber(message.chat.id, message.from_user.username)
         if " " in message.text:
             referrer_candidate = message.text.split()[1]
             if not db.have_users(message.chat.id):
                 db.add_invite(referrer_candidate)
+
+        db.add_subscriber(message.chat.id, message.from_user.username)
 
 
     except:
         pass
 
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    buttons = ["Subscribe", "Contact Admin",'Referral system','Our social media']
+    buttons = ["Subscribe", "Contact Admin", 'Referral system', 'Our social media']
     keyboard.add(*buttons)
 
-    await bot.send_message(message.chat.id,"Okay , let's start")
+    await bot.send_message(message.chat.id, "Okay , let's start")
 
-    await bot.send_message(message.chat.id, 'Click the «subscribe» button to subscribe to  bot ',reply_markup=keyboard)
+    await bot.send_message(message.chat.id, 'Click the «subscribe» button to subscribe to  bot ', reply_markup=keyboard)
 
 
 @dp.message_handler(Text(equals="Contact Admin"))
@@ -56,7 +58,7 @@ async def contact_admin(message: types.Message):
     markup = types.InlineKeyboardMarkup()
     button1 = types.InlineKeyboardButton("Contact Admin", url='https://t.me/diachylum')
     markup.add(button1)
-    await bot.send_message(message.chat.id,'For all questions', reply_markup=markup)
+    await bot.send_message(message.chat.id, 'For all questions', reply_markup=markup)
 
 
 @dp.message_handler(Text(equals='Referral system'))
@@ -71,23 +73,30 @@ async def referal(message: types.Message):
         buttons = ["Create referral code", "How the referral system works?", 'Main menu']
         keyboard.add(*buttons)
         await bot.send_message(message.chat.id, 'Referral account partner ProMint bot',
-                           reply_markup=keyboard)
+                               reply_markup=keyboard)
 
     else:
         keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        buttons = ['View referral code',"Referral statistics", "How the referral system works?", 'Main menu']
+        buttons = ['View referral code', "Referral statistics", "How the referral system works?", 'Main menu']
         keyboard.add(*buttons)
         await bot.send_message(message.chat.id, 'Referral account partner ProMint bot',
                                reply_markup=keyboard)
 
+
 @dp.message_handler(Text(equals='View referral code'))
 async def referal(message: types.Message):
-    await bot.send_message(message.chat.id,f'https://t.me/Binance_nftbox_bot?start={db.return_code(message.chat.id)}')
+    await bot.send_message(message.chat.id, f'https://t.me/Binance_nftbox_bot?start={db.return_code(message.chat.id)}')
 
 
 @dp.message_handler(Text(equals='Referral statistics'))
 async def referal_stas(message: types.Message):
-    await bot.send_message(message.chat.id,f'Unique members total invited:{db.return_users(message.chat.id)}')
+    await bot.send_message(message.chat.id, f'Unique members total invited: {db.return_users(message.chat.id)}\n\n'
+                                            f'Members who combined payment: 0\n\n'
+                                            f'Total earned: $0.00.\n\n'
+                                            f'Withdrawn from the referral system: $0.00.\n\n'
+                                            f'Referral system balance: $0.00')
+
+
 
 @dp.message_handler(Text(equals="Main menu"))
 async def check_media(message: types.Message):
@@ -100,13 +109,16 @@ async def check_media(message: types.Message):
 
 @dp.message_handler(Text(equals="How the referral system works?"))
 async def check_media(message: types.Message):
-    await bot.send_message(message.chat.id,'💰Invite your friends and acquaintances to ProMint Bot, earn real money!\n\nWhat bonuses do we give:\n\n''🧑🏻‍💻 To you:\n'
-                                           'For each referred user who purchases a subscription from the academy, you will receive 10% of the price of their subscription, and you will also receive 10% every time your friend renews their subscription.\n'
-                                           '\n🧑‍💼 To Friend:\n'
-                                           'For entering your referral code, a new user will receive a $5 discount on payment.\n'
-                                           'Payments for the referral system are made once a week in manual mode, after submitting a withdrawal request.')
+    await bot.send_message(message.chat.id,
+                           '💰Invite your friends and acquaintances to ProMint Bot, earn real money!\n\nWhat bonuses do we give:\n\n''🧑🏻‍💻 To you:\n'
+                           'For each referred user who purchases a subscription from the academy, you will receive 10% of the price of their subscription, and you will also receive 10% every time your friend renews their subscription.\n'
+                           '\n🧑‍💼 To Friend:\n'
+                           'For entering your referral code, a new user will receive a $5 discount on payment.\n'
+                           'Payments for the referral system are made once a week in manual mode, after submitting a withdrawal request.')
+
 
 input_code.register_handlers_code(dp)
+
 
 @dp.message_handler(Text(equals="Our social media"))
 async def check_media(message: types.Message):
@@ -122,7 +134,8 @@ async def check_subscription(message: types.Message):
         markup = types.InlineKeyboardMarkup()
         button1 = types.InlineKeyboardButton("Admin", url='https://t.me/diachylum')
         markup.add(button1)
-        await bot.send_message(message.chat.id, "To subscribe , write to the admin in a private message",reply_markup=markup)
+        await bot.send_message(message.chat.id, "To subscribe , write to the admin in a private message",
+                               reply_markup=markup)
     else:
         keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
         buttons = ["Config", 'Start bot']
@@ -131,26 +144,22 @@ async def check_subscription(message: types.Message):
                              reply_markup=keyboard)  # сделать чтоб показывало до какого времени активна
 
 
-
-
-
-#Reload input data
+# Reload input data
 
 
 @dp.message_handler(Text(equals="Config"))
 async def conf(message: types.Message):
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    buttons = ["USER №1", "USER №2", 'USER №3','Reload input data','Сome back']
+    buttons = ["USER №1", "USER №2", 'USER №3', 'Reload input data', 'Сome back']
     keyboard.add(*buttons)
     await bot.send_message(message.chat.id, 'Choose user',
                            reply_markup=keyboard)
 
 
-
-
 input_data.register_handlers_data(dp)
 input_data2.register_handlers_data2(dp)
 input_data3.register_handlers_data3(dp)
+
 
 @dp.message_handler(Text(equals="Сome back"))
 async def conf(message: types.Message):
@@ -164,7 +173,7 @@ async def conf(message: types.Message):
 @dp.message_handler(Text(equals="Start bot"))
 async def conf(message: types.Message):
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    buttons = ["Start 1 account","Start 2 account","Start 3 account", 'Сome back']
+    buttons = ["Start 1 account", "Start 2 account", "Start 3 account", 'Сome back']
     keyboard.add(*buttons)
     await message.answer("We have returned",
                          reply_markup=keyboard)
@@ -174,7 +183,7 @@ async def conf(message: types.Message):
 async def account1(message: types.Message):
     if db.subscriber_exists(message.from_user.id):
         try:
-            selected_box = db.post_product_id(message.chat.id,account='')
+            selected_box = db.post_product_id(message.chat.id, account='')
 
             if headers_is_right(id=message.chat.id):
 
@@ -184,12 +193,12 @@ async def account1(message: types.Message):
                 box = Box(product_id=product_id, amount=selected_box[1], id=message.chat.id)
                 start_sale_time = box._get_start_sale_time
                 send = threading.Thread(target=send_requests_to_buy,
-                                        args=(box,start_sale_time,))
+                                        args=(box, start_sale_time,))
 
                 await bot.send_message(message.chat.id, 'Waiting for start')
                 send.start()
 
-                #await send_requests_to_buy(box, start_sale_time)
+                # await send_requests_to_buy(box, start_sale_time)
 
 
             else:
@@ -207,12 +216,11 @@ async def account1(message: types.Message):
                                add(InlineKeyboardButton("Contact Admin", url='https://t.me/diachylum')))
 
 
-
 @dp.message_handler(Text(equals="Start 2 account"))
 async def account2(message: types.Message):
     if db.subscriber_exists(message.from_user.id):
         try:
-            selected_box = db.post_product_id(message.chat.id,account='2')
+            selected_box = db.post_product_id(message.chat.id, account='2')
 
             if headers_is_right(id=message.chat.id):
 
@@ -222,12 +230,12 @@ async def account2(message: types.Message):
                 box = Box(product_id=product_id, amount=selected_box[1], id=message.chat.id)
                 start_sale_time = box._get_start_sale_time
                 send = threading.Thread(target=send_requests_to_buy,
-                                        args=(box,start_sale_time,))
+                                        args=(box, start_sale_time,))
 
                 await bot.send_message(message.chat.id, 'Waiting for start')
                 send.start()
 
-                #await send_requests_to_buy(box, start_sale_time)
+                # await send_requests_to_buy(box, start_sale_time)
 
 
             else:
